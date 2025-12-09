1974183e12ccf7f447262e94c1fbc7ea9c0801ea
Lisandro Alarcon <lisandroaalarcon@gmail.com>
2025-12-09 10:21:57 -0300

    feat: Agregar endpoint hello

--- docker-api/main.py
+++ docker-api/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/test/{endpoint}", status_code=201)
 def test_endpoint(endpoint: str):
     return endpoint
+
+@app.get("/hello", status_code=200)
+def hello_endpoint():
+    return "Hola"
