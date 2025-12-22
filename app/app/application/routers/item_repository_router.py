74a34a19a62635a8900f1da4dee45dba5f07c12b
Lisandro Alarcon <lisandroaalarcon@gmail.com>
2025-12-22 12:11:10 -0300

    feat: Return schemas from endpoints

--- app/app/application/routers/item_repository_router.py
+++ app/app/application/routers/item_repository_router.py
@@ -1,18 +1,24 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 from app.application.usecases.get_item_by_id import GetItemByIdUseCase
+
+class ItemSchema(BaseModel):
+    id: int
+    description: str
+    price: float
 
 def create_item_repository_router(get_item_by_id: GetItemByIdUseCase) -> APIRouter:
     router = APIRouter()
 
     @router.get("/item/{item_id}")
-    def get_item(item_id: int) -> dict:
+    def get_item(item_id: int) -> ItemSchema:
         item = get_item_by_id.execute(item_id)
 
-        return {
-            "id": item.get_id(),
-            "description": item.get_description(),
-            "price": item.get_price()
-        }
+        return ItemSchema(
+            id=item.get_id(),
+            description=item.get_description(),
+            price=item.get_price()
+        )
 
     return router
--- app/app/application/routers/order_repository_router.py
+++ app/app/application/routers/order_repository_router.py
@@ -1,25 +1,24 @@
-import logging
-
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 from app.application.usecases.get_order_by_id import GetOrderByIdUseCase
+
+class OrderSchema(BaseModel):
+    id: int
+    customer_id: int
+    total: float
 
 def create_order_repository_router(get_order_by_id: GetOrderByIdUseCase) -> APIRouter:
     router = APIRouter()
 
     @router.get("/order/{order_id}")
-    def get_order(order_id: int) -> dict:
+    def get_order(order_id: int) -> OrderSchema:
         order = get_order_by_id.execute(order_id)
 
-        logging.basicConfig(level=logging.INFO)
-        logger = logging.getLogger(__name__)
-
-        logger.info(f"Order retrieved: ID={order.get_order_id()}, CustomerID={order.get_customer_id()}, Total={order.get_total()}")
-
-        return {
-            "id": order.get_order_id(),
-            "customer_id": order.get_customer_id(),
-            "total": order.get_total()
-        }
+        return OrderSchema(
+            id=order.get_order_id(),
+            customer_id=order.get_customer_id(),
+            total=order.get_total()
+        )
 
     return router
